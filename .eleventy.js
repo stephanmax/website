ff8105467b9059502651fbf9ad75ff9cce0e08ab
Stephan Max <hello@stephanmax.com>
2022-07-04 19:19:33 +0200

    Add RSS feed, npm audit fix

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,15 @@
+const pluginRss = require("@11ty/eleventy-plugin-rss");
+
 module.exports = function(eleventyConfig) {
+  // Plugins
+  eleventyConfig.addPlugin(pluginRss);
+
+  // Filters
   eleventyConfig.addNunjucksFilter('ISOString', function(val) {
     return val.toISOString().substring(0, 10);
   });
 
+  // General Config
   return {
     dir: {
       input: 'src',
